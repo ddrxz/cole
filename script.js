41ade3a3b9455941b0a5ccdf30379013205baf7d
ddrxz <gastonferrari98@gmail.com>
2025-04-21 09:01:56 -0300

    All change and fix bug

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-function generarPDF() {
+function generarPDF(elementId, filename) {
+    const botonPDF = document.querySelector('.btn-danger'); 
+    if (botonPDF) {
+        botonPDF.style.display = 'none'; 
+    }
+
     const opciones = {
-      margin:       1,
-      filename:     'horarios_colectivos.pdf',
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        margin: 1,
+        filename: filename + '.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
     };
 
-    // Genera el PDF usando el contenido de la tabla con el id "horarios-table"
-    const element = document.getElementById('horarios-table');
-    html2pdf().from(element).set(opciones).save();
-  }
+    const element = document.getElementById(elementId);
+    if (element) {
+        html2pdf().from(element).set(opciones).save().finally(() => {
+            if (botonPDF) {
+                botonPDF.style.display = 'inline-block'; 
+            }
+        });
+    } else {
+        console.error(`No se encontró el elemento con el ID '${elementId}'.`);
+        if (botonPDF) {
+            botonPDF.style.display = 'inline-block'; 
+        }
+    }
+}
